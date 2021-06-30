605b1d0decf0771648780e30ce52ad5b561030d2
Chris Trout <chris@mytrout.com>
2021-06-29 23:51:49 -0400

    Correct path-based unit tests to work with Linux servers.

--- Steps/IO/Command/test/MyTrout.Pipelines.Steps.IO.Command.Tests/ExecuteCommandStepTests.cs
+++ Steps/IO/Command/test/MyTrout.Pipelines.Steps.IO.Command.Tests/ExecuteCommandStepTests.cs
@@ -1,4 +1,27 @@
-﻿
+﻿// <copyright file="ExecuteCommandStepTests.cs" company="Chris Trout">
+// MIT License
+//
+// Copyright(c) 2021 Chris Trout
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
 namespace MyTrout.Pipelines.Steps.IO.Command.Tests
 {
     using Microsoft.Extensions.Logging;
@@ -50,7 +73,7 @@ namespace MyTrout.Pipelines.Steps.IO.Command.Tests
             var options = new ExecuteCommandOptions()
             {
                 Arguments = "--exception",
-                CommandString = Directory.GetParent(Assembly.GetExecutingAssembly().Location) + "\\ExceptionConsole.exe",
+                CommandString = $"{Directory.GetParent(Assembly.GetExecutingAssembly().Location)}{Path.DirectorySeparatorChar}ExceptionConsole.exe",
                 ExpectedResult = "Nothing",
                 IncludeFileNameTransformInArguments = false
             };
@@ -92,7 +115,7 @@ namespace MyTrout.Pipelines.Steps.IO.Command.Tests
             var options = new ExecuteCommandOptions()
             {
                 Arguments = "{0}",
-                CommandString = Directory.GetParent(Assembly.GetExecutingAssembly().Location) + "\\ExceptionConsole.exe",
+                CommandString = $"{Directory.GetParent(Assembly.GetExecutingAssembly().Location)}{Path.DirectorySeparatorChar}ExceptionConsole.exe",
                 ExpectedResult = "Nothing",
                 IncludeFileNameTransformInArguments = true
             };
@@ -126,13 +149,13 @@ namespace MyTrout.Pipelines.Steps.IO.Command.Tests
             var options = new ExecuteCommandOptions()
             {
                 Arguments = "Something",
-                CommandString = Directory.GetParent(Assembly.GetExecutingAssembly().Location) + "\\ExceptionConsole.exe",
+                CommandString = $"{Directory.GetParent(Assembly.GetExecutingAssembly().Location)}{Path.DirectorySeparatorChar}ExceptionConsole.exe",
                 ExpectedResult = "Nothing",
                 IncludeFileNameTransformInArguments = false
             };
 
             PipelineContext context = new PipelineContext();
-            context.Items.Add(FileConstants.SOURCE_FILE, Directory.GetParent(Assembly.GetExecutingAssembly().Location) + "\\ExecuteCommandStepTests.cs");
+            context.Items.Add(FileConstants.SOURCE_FILE, $"{Directory.GetParent(Assembly.GetExecutingAssembly().Location)}{Path.DirectorySeparatorChar}ExecuteCommandStepTests.cs");
 
             var mockNext = new Mock<IPipelineRequest>();
             mockNext.Setup(x => x.InvokeAsync(context)).Returns(Task.CompletedTask);
