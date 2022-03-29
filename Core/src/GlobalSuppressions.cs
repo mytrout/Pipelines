f9afddbb609c63a579c304d17965ef9fbfe6768b
Chris Trout <chris@mytrout.com>
2022-03-28 22:05:06 -0400

    Suppress LINQ Expression warning because the existing code is simpler to understand than the replacement LINQ expression.

--- Core/src/GlobalSuppressions.cs
+++ Core/src/GlobalSuppressions.cs
@@ -1,7 +1,7 @@
 ﻿// <copyright file="GlobalSuppressions.cs" company="Chris Trout">
 // MIT License
 //
-// Copyright(c) 2019-2020 Chris Trout
+// Copyright(c) 2019-2022 Chris Trout
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -26,3 +26,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "Constant names should contain underscores.", Scope = "member", Target = "~F:MyTrout.Pipelines.Steps.PipelineContextConstants.INPUT_STREAM")]
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "Constant names should contain underscores.", Scope = "member", Target = "~F:MyTrout.Pipelines.Steps.PipelineContextConstants.OUTPUT_STREAM")]
+[assembly: SuppressMessage("Minor Code Smell", "S3267:Loops should be simplified with \"LINQ\" expressions", Justification = "This code doesn't have a simple LINQ expression conversion.", Scope = "member", Target = "~M:MyTrout.Pipelines.Steps.RenameContextItemStep.InvokeCachedCoreAsync(MyTrout.Pipelines.IPipelineContext)~System.Threading.Tasks.Task")]
