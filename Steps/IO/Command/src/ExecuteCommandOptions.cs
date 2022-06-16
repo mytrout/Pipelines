7401d071436c6425899b0f9cd26445302c1a819f
simkin2004 <chris@mytrout.com>
2022-06-16 07:28:54 -0400

    Feature/upgrade steps io command to pipelines 4 (#171)
    * Upgrade to Pipelines v4.0.0
    
    * Upgrade Steps.IO.Command to Pipelines v4.0.0
    
    * Update analyzers and readme.md
    
    * Alter build to handle .NET 7.0

--- Steps/IO/Command/src/ExecuteCommandOptions.cs
+++ Steps/IO/Command/src/ExecuteCommandOptions.cs
@@ -1,7 +1,7 @@
 ﻿// <copyright file="ExecuteCommandOptions.cs" company="Chris Trout">
 // MIT License
 //
-// Copyright(c) 2021 Chris Trout
+// Copyright(c) 2021-2022 Chris Trout
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -37,6 +37,21 @@ namespace MyTrout.Pipelines.Steps.IO.Command
         public string Arguments { get; set; } = string.Empty;
 
         /// <summary>
+        /// Gets or sets the Pipeline Context Name for a Command Line Status.
+        /// </summary>
+        public string CommandLineStatusContextName { get; set; } = CommandLineConstants.COMMAND_LINE_STATUS;
+
+        /// <summary>
+        /// Gets or sets the Pipeline Context Name for a Command Line Failed Status.
+        /// </summary>
+        public string CommandLineFailedStatusContextName { get; set; } = CommandLineConstants.COMMAND_LINE_FAILED;
+
+        /// <summary>
+        /// Gets or sets the Pipeline Context Name for a Command Line Successful Status.
+        /// </summary>
+        public string CommandLineSuccessStatusContextName { get; set; } = CommandLineConstants.COMMAND_LINE_SUCCESS;
+
+        /// <summary>
         /// Gets or sets the full path and file name to execute the command line utility.
         /// </summary>
         public virtual string CommandString { get; set; } = string.Empty;
@@ -47,8 +62,13 @@ namespace MyTrout.Pipelines.Steps.IO.Command
         public string ExpectedResult { get; set; } = string.Empty;
 
         /// <summary>
-        /// Gets or sets a value indicating whether <see cref="Arguments"/> has a replaceable parameter for <see cref="FileConstants.SOURCE_FILE"/>.
+        /// Gets or sets a value indicating whether <see cref="Arguments"/> has a replaceable parameter for <see cref="TargetFileContextName"/>.
         /// </summary>
         public bool IncludeFileNameTransformInArguments { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the command line's target file for execution.
+        /// </summary>
+        public string TargetFileContextName { get; set; } = FileConstants.TARGET_FILE;
     }
 }
